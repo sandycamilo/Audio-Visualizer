24a0ea3503210fcfd17d13d28c72a97032da2c56
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2023-11-20 22:50:03 -0500

    u

--- main.js
+++ main.js
@@ -54,31 +54,36 @@ function startAudio() {
 function makeDraggable(element) {
   let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-  element.onmousedown = dragMouseDown;
+	element.addEventListener('mousedown', dragMouseDown);
+  element.addEventListener('touchstart', dragMouseDown);
 
   function dragMouseDown(e) {
-    e = e || window.event;
+		e = e || window.event;
     e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+    pos3 = e.clientX || e.touches[0].clientX;
+    pos4 = e.clientY || e.touches[0].clientY;
     document.onmouseup = closeDragElement;
+    document.ontouchend = closeDragElement;
     document.onmousemove = elementDrag;
+    document.ontouchmove = elementDrag;
   }
 
   function elementDrag(e) {
-    e = e || window.event;
+		e = e || window.event;
     e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+    pos1 = pos3 - (e.clientX || e.touches[0].clientX);
+    pos2 = pos4 - (e.clientY || e.touches[0].clientY);
+    pos3 = e.clientX || e.touches[0].clientX;
+    pos4 = e.clientY || e.touches[0].clientY;
     element.style.top = (element.offsetTop - pos2) + 'px';
     element.style.left = (element.offsetLeft - pos1) + 'px';
   }
 
   function closeDragElement() {
     document.onmouseup = null;
+    document.ontouchend = null;
     document.onmousemove = null;
+    document.ontouchmove = null;
   }
 }
 
